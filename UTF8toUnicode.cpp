6f5dc30f81a0de8cf4614084c80dad5879508d11
Dmitry Leskov <git@dmitryleskov.com>
2011-11-25 14:12:46 +0700

    Simplified by using _setmode on both stdin and stdout, enabled secure template overloads

--- UTF8toUnicode.cpp
+++ UTF8toUnicode.cpp
@@ -1,44 +1,26 @@
-#include <windows.h>
-#include <stdio.h>
+#define _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES 1
+
 #include <fcntl.h>
 #include <io.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void) {
-	char in[1024];
-	WCHAR wBuf[1024];
+	wchar_t buf[1024];
+	if (_setmode(_fileno(stdin), _O_U8TEXT) == -1) {
+		perror("Cannot set stdin to UTF-8");
+		exit(1);
+	}
 	if (_setmode(_fileno(stdout), _O_U16TEXT) == -1) {
 		perror("Cannot set stdout to Unicode");
 		exit(1);
 	}
-	while (gets(in)) {
-		int wLen =
-			MultiByteToWideChar(
-			CP_UTF8,
-			MB_ERR_INVALID_CHARS,
-			in,
-			-1,
-			wBuf,
-			1024);
-		if (!wLen) { 
-			DWORD err = GetLastError();
-			LPTSTR msg;
-			if (FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |
-				FORMAT_MESSAGE_FROM_SYSTEM,
-				NULL,
-				err,
-				MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-				(LPTSTR)&msg,
-				0,
-				NULL) == 0) {
-			    fprintf(stderr, "Unknown error 0x%x\n", err);
-			} else {
-				CharToOem(msg, msg);  // Would fail if UNICODE is defined
-				fprintf(stderr, msg);
-			}
+	while (_getws(buf)) {
+		if (wcschr(buf, L'\xfffd')) {
+			fprintf(stderr, "Error: Input stream not in UTF-8\n");
 			exit(1);
 		}
-		else {
-			_putws(wBuf);
-		}
+		_putws(buf);
 	}
 }
