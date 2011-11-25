1e8b1593f8b7a3794be603ffefcdb925b505c19c
Dmitry Leskov <git@dmitryleskov.com>
2011-11-25 13:42:44 +0700

    stderr left intact, so error message has to be converted to OEM CP.

--- UTF8toUnicode.cpp
+++ UTF8toUnicode.cpp
@@ -1,10 +1,6 @@
-#ifndef UNICODE
-#define UNICODE
-#endif
-
 #include <windows.h>
 #include <stdio.h>
-#include <fcntl.h>
+//#include <fcntl.h>
 #include <io.h>
 
 int main(void) {
@@ -12,10 +8,6 @@ int main(void) {
 	WCHAR wBuf[1024];
 	if (_setmode(_fileno(stdout), _O_U16TEXT) == -1) {
 		perror("Cannot set stdout to Unicode");
-		exit(1);
-	}
-	if (_setmode(_fileno(stderr), _O_U16TEXT) == -1) {
-		perror("Cannot set stderr to Unicode");
 		exit(1);
 	}
 	while (gets(in)) {
@@ -29,18 +21,20 @@ int main(void) {
 			1024);
 		if (!wLen) { 
 			DWORD err = GetLastError();
-			LPWSTR msg;
-			if(FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |
+			LPTSTR msg;
+			if (FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |
 				FORMAT_MESSAGE_FROM_SYSTEM,
 				NULL,
 				err,
 				MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-				(LPWSTR)&msg,
+				(LPTSTR)&msg,
 				0,
 				NULL) == 0) {
-			    fwprintf(stderr, L"Unknown error 0x%x\n", err);
-		    }
-			fwprintf(stderr, msg);
+			    fprintf(stderr, "Unknown error 0x%x\n", err);
+			} else {
+				CharToOem(msg, msg);  // Would fail if UNICODE is defined
+				fprintf(stderr, msg);
+			}
 			exit(1);
 		}
 		else {
