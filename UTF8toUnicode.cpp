0b0d87d026e27b39ff4584fe964992ec8dd513ca
Dmitry Leskov <git@dmitryleskov.com>
2011-11-25 13:30:30 +0700

    Using Unicode stdout and stderr

--- UTF8toUnicode.cpp
+++ UTF8toUnicode.cpp
@@ -1,14 +1,23 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
 #include <windows.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <io.h>
 
-int main(int argc, char* argv[]) {
+int main(void) {
 	char in[1024];
 	WCHAR wBuf[1024];
-	if (argc < 2) {
-		puts("Usage: UTF8toUnicode out <in");
+	if (_setmode(_fileno(stdout), _O_U16TEXT) == -1) {
+		perror("Cannot set stdout to Unicode");
 		exit(1);
 	}
-	FILE *outfile = fopen(argv[1], "wb");
+	if (_setmode(_fileno(stderr), _O_U16TEXT) == -1) {
+		perror("Cannot set stderr to Unicode");
+		exit(1);
+	}
 	while (gets(in)) {
 		int wLen =
 			MultiByteToWideChar(
@@ -20,18 +29,22 @@ int main(int argc, char* argv[]) {
 			1024);
 		if (!wLen) { 
 			DWORD err = GetLastError();
-			puts("Error");
+			LPWSTR msg;
+			if(FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |
+				FORMAT_MESSAGE_FROM_SYSTEM,
+				NULL,
+				err,
+				MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+				(LPWSTR)&msg,
+				0,
+				NULL) == 0) {
+			    fwprintf(stderr, L"Unknown error 0x%x\n", err);
+		    }
+			fwprintf(stderr, msg);
 			exit(1);
-			if (err == ERROR_NO_UNICODE_TRANSLATION) { // Not UTF-8
-				puts(in);
-				continue;
-			}
-			else exit(err);
 		}
 		else {
-			// Do not write the zero terminator
-			fwrite(wBuf, sizeof(WCHAR), wLen-1, outfile);
-			fwrite(L"\x000d\x000a", sizeof(WCHAR), 2, outfile);
+			_putws(wBuf);
 		}
 	}
 }
