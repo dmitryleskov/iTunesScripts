63522b8a19d744a127a1a3e2fb9dae191a1886c9
Dmitry Leskov <git@dmitryleskov.com>
2011-12-07 00:00:36 +0700

    Invalid parameter handling

--- UTF8to16.cpp
+++ UTF8to16.cpp
@@ -1,6 +1,8 @@
 //
 // UTF8to16.cpp - convert a Unicode text stream from UTF-8 to UTF-16
 //
+// Limitation: input stream may not contain lines longer than 1024 bytes
+//
 // Version: 0.9
 //
 // Refer to the file COPYING.BSD for licensing conditions.
@@ -9,6 +11,7 @@
 //
 
 #define _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES 1
+#include <crtdbg.h>  // For _CrtSetReportMode
 
 #include <fcntl.h>
 #include <io.h>
@@ -16,8 +19,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+void invalidParameterHandler(
+	const wchar_t* expression,
+	const wchar_t* function,
+	const wchar_t* file,
+	unsigned int line,
+	uintptr_t pReserved) {
+#ifdef _DEBUG
+	fwprintf(stderr, L"Invalid parameter detected in function %s."
+		L" File: %s Line: %d\n", function, file, line);
+	fwprintf(stderr, L"Expression: %s\n", expression);
+#else
+	fputws(L"Invalid parameter detected (buffer overflow?), aborting.", stderr);
+#endif
+}
+
 int main(void) {
 	wchar_t buf[1024];
+	_invalid_parameter_handler oldHandler, newHandler;
+	newHandler = invalidParameterHandler;
+	oldHandler = _set_invalid_parameter_handler(newHandler);
+	// Disable the message box for assertions when linked with debug CRT library.
+	_CrtSetReportMode(_CRT_ASSERT, 0);
+
 	if (_setmode(_fileno(stdin), _O_U8TEXT) == -1) {
 		perror("Cannot set stdin to UTF-8");
 		exit(1);
