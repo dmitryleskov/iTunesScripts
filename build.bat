c4dae0ae41ea6e4a8f2452c5f2cc94292d19c62d
Dmitry Leskov <git@dmitryleskov.com>
2011-12-10 12:59:26 +0700

    Added FLAC binaries, licenses, build script

---
+++ build.bat
@@ -0,0 +1,8 @@
+if exist dist rmdir /s /q dist
+mkdir dist
+call src\utf8to16\build.bat
+"C:\Program Files (x86)\Pandoc\bin\pandoc.exe" -s -t plain -o dist\README.txt README.md
+"C:\Program Files (x86)\Pandoc\bin\pandoc.exe" -s --html5 -o dist\README.html README.md
+for %%i in (AddFLACs.bat AddFLACs.js COPYING.BSD COPYING.GPL bin\flac.exe bin\metaflac.exe) do copy %%i dist
+cd dist 
+zip AddFLACs-0.1.zip *
--- src/UTF8to16/build.bat
+++ src/UTF8to16/build.bat
@@ -8,11 +8,11 @@ if not exist "%vs90comntools%\vsvars32.bat" goto novc
 call "%vs90comntools%\vsvars32.bat"
 if not exist "%vcinstalldir%\vcpackages\vcbuild.exe" goto novc
 
-"%vcinstalldir%\vcpackages\vcbuild.exe" UTF8to16.sln "Release|Win32"
+"%vcinstalldir%\vcpackages\vcbuild.exe" %~dp0UTF8to16.sln "Release|Win32"
 if %errorlevel% neq 0 goto quit
-copy Release\UTF8to16.exe
-"%vcinstalldir%\vcpackages\vcbuild.exe" /c UTF8to16.sln "Release|Win32"
-rmdir Release
+copy %~dp0\Release\UTF8to16.exe %~dp0\..\..\dist
+"%vcinstalldir%\vcpackages\vcbuild.exe" /c %~dp0UTF8to16.sln "Release|Win32"
+rmdir %~dp0Release
 goto quit
 
 :novc
