dacd6ba24deabdd4c804b4e89648d59de1e9ec9c
Dmitry Leskov <git@dmitryleskov.com>
2013-07-14 13:50:22 +0700

    Bump version to 0.93 and make it a "config" variable.

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+@echo off
+set version=0.93
 if exist dist rmdir /s /q dist
 mkdir dist
 call src\utf8to16\build.bat
@@ -5,5 +7,5 @@ call src\utf8to16\build.bat
 "C:\Program Files (x86)\Pandoc\bin\pandoc.exe" -s --html5 -o dist\README.html README.md
 for %%i in (AddFLACs.bat AddFLACs.js COPYING.MIT COPYING.GPL bin\flac.exe bin\metaflac.exe) do copy %%i dist
 cd dist
-if exist ..\AddFLACs-0.92.zip del ..\AddFLACs-0.92.zip
-zip ..\AddFLACs-0.92.zip *
+if exist ..\AddFLACs-%version%.zip del ..\AddFLACs-%version%.zip
+zip ..\AddFLACs-%version%.zip *
